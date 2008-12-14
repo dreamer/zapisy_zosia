7fd9c57a2ec6f92edde3af7a27eb2eff359ab361
dreamer_ <dreamer.tan@gmail.com>
2008-12-14 02:02:50 +0100

    slight changes regarding login

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,18 @@ from django.shortcuts import render_to_response
 from common.forms import LoginForm
 from models import *
 
+def main_view(view):
+    def new_view(*args):
+        locals = view(*args)
+        locals['user']       = args[0].user
+        locals['title']      = "Blog"
+        locals['login_form'] = LoginForm()
+        return render_to_response('blog.html',locals)
+    return new_view
+
+@main_view
 def index(request):
-    login_form = LoginForm()
-    title = "Blog"
     user = request.user
     blog_posts = BlogPost.objects.all()
-    return render_to_response('blog.html',locals())
+    return locals()
 
