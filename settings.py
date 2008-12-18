d74e993dea13041165aef87fdce190d3f1438dca
dreamer_ <dreamer.tan@gmail.com>
2008-12-18 17:41:16 +0100

    i18n introduction

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ TIME_ZONE = 'CET'
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl'
 
 SITE_ID = 1
 
