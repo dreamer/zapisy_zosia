5eada3edc0c759de62943bb6d89674937a9e3f16
dreamer_ <dreamer.tan@gmail.com>
2008-12-24 00:57:38 +0100

    feedback from login

--- common/views.py
+++ common/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render_to_response, HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import *
-
 from django.contrib.auth.models import User
+from forms import *
 
 def login_view(request):
-    email = request.POST['email']
-    password = request.POST['password']
-    user = authenticate(username=email, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request,user)
-        else:
-            print 'disabled account'
-    else:
-        print 'invalid login'
-    return HttpResponseRedirect('/blog/')
+    title = "Login"
+    user = request.user
+    if not user.is_authenticated():
+        form = LoginForm()
+        try:
+            reason = None # theistic code ;)
+            greetings_stranger = False
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                else:
+                    reason = 'LOGIN_account_disabled'
+            else:
+                reason = 'LOGIN_invalid'
+        except Exception:
+            greetings_stranger = True
+        if not reason and not greetings_stranger:
+            # login quietly
+            return HttpResponseRedirect('/blog/')
+    # this template acutally is used only in case of unsuccesfull login
+    return render_to_response('login.html', locals())
 
 def logout_view(request):
     logout(request)
