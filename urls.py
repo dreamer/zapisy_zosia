1082b2496a6c43f3541f7319bda3e8f4e3bda8cf
dreamer_ <dreamer.tan@gmail.com>
2008-12-23 23:33:37 +0100

    backlog update; rooms disabled

--- urls.py
+++ urls.py
@@ -22,20 +22,27 @@ urlpatterns = patterns('',
      (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed',
         {'feed_dict': feeds}),
 
+     # admin related
      (r'^admin/(.*)', admin.site.root),
 
+     # registration related
      (r'^register/$', registration.views.register),
      (r'^register/thanks/', registration.views.thanks),
+     (r'^register/recover/$', registration.views.recover),
+     # (r'^register/add_org/$', registration.views.add_organization),
 
+     # login / logout
      (r'^logout/$', common.views.logout_view),
      (r'^login/$', common.views.login_view),
-     # (r'^register/add_org/$', registration.views.add_organization),
-     (r'^register/recover/$', registration.views.recover),
 
+     # apps main urls
      (r'^blog/$', blog.views.index),
      (r'^lectures/$', lectures.views.index),
-     (r'^rooms/$', rooms.views.index),
+     # temporarly disabled
+     # (r'^rooms/$', rooms.views.index),
 
+     # static media
+     # note, that thid should be disabled for production code
      (r'^static_media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': os.getcwd()+os.sep+'static_media', 'show_indexes': True}),
 )
