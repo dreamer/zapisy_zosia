3c04da31da431f23065ea94d612a46f5f60d221c
dreamer_ <dreamer.tan@gmail.com>
2009-01-07 03:42:39 +0100

    consider it lorem ipsum

--- lectures/models.py
+++ lectures/models.py
@@ -7,15 +7,15 @@ class Lecture(models.Model):
     title     = models.CharField(max_length=128)
     duration  = models.PositiveIntegerField(max_length=3)
     abstract  = models.TextField(max_length=2048)
-    info      = models.TextField(max_length=2048, blank=True, null=True) 
+    info      = models.TextField(max_length=2048, blank=True, null=True)
     #dodatkowe info dla organizatorów
-    author     = models.CharField(max_length=128)
+    author    = models.CharField(max_length=128)
     #author    = models.ForeignKey(User, unique=False)
     date_time = models.DateTimeField()
     accepted  = models.BooleanField()
 
-    #def __str__(self):
-    #    return "%s - %s" % ( self.title, self.author )
+    def __str__(self):
+        return "%s - %s" % ( self.title, "anonim")
 
     #class Meta:
     #    ordering = ["title"]
