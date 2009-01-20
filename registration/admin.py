b7be388b84c832a16e1202e472abfe5994765b7f
dreamer_ <dreamer.tan@gmail.com>
2009-01-20 17:34:42 +0100

    ii, ksi links; other small changes

--- registration/admin.py
+++ registration/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from models import *
 
 class UserPreferencesAdmin(admin.ModelAdmin):
-    list_display = ('user', 'day_1', 'day_2', 'day_3',
+    list_display = (
+    'user',
+    'paid',
+    'day_1',
+    'day_2',
+    'day_3',
     'breakfast_2',
     'breakfast_3',
     'breakfast_4',    
@@ -11,7 +16,6 @@ class UserPreferencesAdmin(admin.ModelAdmin):
     'dinner_3',
     'bus',
     'vegetarian',
-    'paid',
     'shirt_size',
     'shirt_type',
     'org',
