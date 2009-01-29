33b53cd4c58874f97430e2b853dbfd956daef545
dreamer_ <dreamer.tan@gmail.com>
2009-01-29 21:15:45 +0100

    slight model change; new branch

--- rooms/models.py
+++ rooms/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Room(models.Model):
     number = models.CharField(max_length=16)
     capacity = models.PositiveIntegerField(max_length=1) # burżuje jesteśmy :P
-    # locators
+    # locators = models.fields blah
+    # owner = ... user
     # password
     # unlock_time
 
