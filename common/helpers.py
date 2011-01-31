8444dd8ebced1fe8c5bea6783a6d34bf95c8f2e6
Patryk Obara <dreamer.tan@gmail.com>
2011-01-31 04:58:20 +0100

    Część danych poprawionych na dane dla zosi 2011

--- common/helpers.py
+++ common/helpers.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 def is_registration_enabled():
 	# this is the only place that should be changed
-	start_date = datetime(2010,2,1, 1,05)
-	final_date = datetime(2011,2,22, 1,05)
+	start_date = datetime(2011,2,2, 1,05)
+	final_date = datetime(2011,2,24, 1,05)
 	assert start_date < final_date
 	return datetime.now() > start_date and datetime.now() < final_date
 
@@ -17,8 +17,8 @@ def is_registration_disabled():
 
 def is_lecture_suggesting_enabled():
 	# this is the only place that should be changed
-	start_date = datetime(2010,2,1, 1,05)
-	final_date = datetime(2011,3,2, 1,05)
+	start_date = datetime(2011,2,2, 1,05)
+	final_date = datetime(2011,2,24, 1,05)
 	assert start_date < final_date
 	return datetime.now() > start_date and datetime.now() < final_date
 
@@ -28,7 +28,7 @@ def is_lecture_suggesting_disabled():
 
 
 def is_rooming_enabled():
-	start_date = datetime(2010,2,27, 20,00)
+	start_date = datetime(2011,2,26, 20,00)
 	final_date = datetime(2011,3,1, 20,00)
 	assert start_date < final_date
 	return datetime.now() > start_date and datetime.now() < final_date
