836c6804a9c674ccd4096985c4820e1fd7d1c9cf
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-02-21 17:48:41 +0100

    Added duration to list display in admin panel(Lectures)

--- lectures/admin.py
+++ lectures/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from models import *
 
 class LectureAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'accepted', 'date_time')
+    list_display = ('title', 'author','duration', 'accepted', 'date_time')
 
 admin.site.register(Lecture, LectureAdmin)
