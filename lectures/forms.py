f730a7349e2e8389236ad4b44057a345e9b8608b
Małgorzata Jurkiewicz <gosia.jurkiewicz@gmail.com>
2011-02-22 17:06:27 +0100

    [ZOSIA11] Back without 45-minutes lectures

--- lectures/forms.py
+++ lectures/forms.py
@@ -4,7 +4,7 @@ from django import forms
 
 class NewLectureForm(forms.Form):
     title = forms.CharField( max_length=128, required=True )
-    duration = forms.ChoiceField(choices = ([(5,5), (15,15),(20,20),(25,25),(30,30), (45,45), (100,'inne')]),) 
+    duration = forms.ChoiceField(choices = ([(5,5), (15,15),(20,20),(25,25),(30,30), (100,'inne')]),) 
     abstract = forms.CharField( widget=forms.Textarea(),
                                 required=True, max_length=512 )
     sprezentujpl_email = forms.EmailField(required=False)
