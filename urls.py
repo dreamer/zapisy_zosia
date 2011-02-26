027c273db18f2fe29c1084ac832bf48f27d6bd2d
Patryk Obara <dreamer.tan@gmail.com>
2011-02-26 15:42:10 +0100

    Revert "new url"
    This reverts commit 5f6d686093315a25ad99c7ddd1802685adcba611.

--- urls.py
+++ urls.py
@@ -53,7 +53,6 @@ urlpatterns = patterns('',
      (r'^register/activate/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', registration.views.activate_user),
 
      (r'^change_preferences/$', registration.views.change_preferences),
-     (r'^preferencje/$', registration.views.change_preferences),
 
      # login / logout
      (r'^login/$', common.views.login_view),
